037809d571d55f5fde77459762c4879f1b398018
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-31 02:59:36 -0400

    Added functions for both dropdown menu, added highlighting for currently selected item and variable to keep track of what was last selected.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  var program = 'SEARCH ALL', education = 'SEARCH ALL';
+  $('.program-list li a:contains("' + program + '")').css('background-color', 'red');
+  $('.education-list li a:contains("' + program + '")').css('background-color', 'red');
+  $('.program-list li a').click(function() {
+    $('.program-list li a:contains("' + program + '")').css('background-color', '');
+    program = $(this).text();
+    $('.program-list li a:contains("' + program + '")').css('background-color', 'red');
+  });
+  $('.education-list li a').click(function() {
+    $('.education-list li a:contains("' + education + '")').css('background-color', '');
+    education = $(this).text();
+    $('.education-list li a:contains("' + education + '")').css('background-color', 'red');
+  });
 });
